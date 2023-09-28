4f78e536fc2d6309845ea4eea7806ea5734c8933
Damini Kesharkar <daminisk272@gmail.com>
2023-09-29 00:43:03 +0530

    Added GetElementByTAgName code

--- DomManipulation/script.js
+++ DomManipulation/script.js
@@ -24,3 +24,6 @@ items[2].style.backgroundColor = "green";
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight = "bold";
 }
+
+var li = document.getElementsByTagName("li");
+console.log(li[4].innerText);
