7457b049d06040f71bcbd0fa724a15439fe00284
Damini Kesharkar <daminisk272@gmail.com>
2023-09-28 22:49:04 +0530

    Created simple registration page

---
+++ Registration-page/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("registration");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+        const username = document.getElementById("username").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+
+        console.log("Username:", username);
+        console.log("Email:", email);
+        console.log("Password:", password);
+
+        form.reset();
+    });
+});
