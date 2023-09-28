ec004c393338b3cb71b3a69fb71eff7c8e5bb670
Damini Kesharkar <daminisk272@gmail.com>
2023-09-28 23:46:01 +0530

    Added DOM manipulation code

--- DomManipulation/script.js
+++ DomManipulation/script.js
@@ -18,3 +18,9 @@ console.log(addItem.childNodes[1]);
 addItem.childNodes[1].style.fontWeight = "bold";
 addItem.childNodes[1].style.color = "green";
 
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "green";
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight = "bold";
+}
