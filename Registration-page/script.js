9c17c69ea7bd0d2ba364cc66450a3f0d7b422d05
Damini Kesharkar <daminisk272@gmail.com>
2023-09-29 17:42:27 +0530

    added display and delete user feature

--- Registration-page/script.js
+++ Registration-page/script.js
@@ -18,10 +18,59 @@ document.addEventListener("DOMContentLoaded", function() {
             email : email,
             password : password
         };
-        console.log(userData);
-
+        
         const userDataJson = JSON.stringify(userData);
         localStorage.setItem(userData.email,userDataJson);
         form.reset();
+
+        addUsers(userData.name,userData.email);
     });
+
+    function addUsers(name,email){
+
+        var newUserList = document.getElementById("newusers");
+
+        var li = document.createElement("li");
+        li.className="userList";
+
+        var btn = document.createElement("button");
+        btn.className = "btn delete";
+
+        var text = document.createTextNode("Username: "+name+" Email: "+email);
+        li.appendChild(text);
+
+
+        btn.appendChild(document.createTextNode("Delete"));
+        li.appendChild(btn);
+
+        newUserList.appendChild(li);        
+    }
+
+    function displayUsers(){        
+        for(var i=0;i<localStorage.length;i++){
+            const key = localStorage.key(i); //email
+            const value = JSON.parse(localStorage.getItem(key));
+            addUsers(value.name,key);
+            console.log(value.name,key);        
+        }
+    }
+
+    var ul = document.getElementById("newusers");
+    ul.addEventListener("click",deleteUser);
+
+    function deleteUser(e){    
+        if(e.target.classList.contains("delete")){            
+                var li = e.target.parentElement;
+                var text = li.firstChild.textContent;
+                const emailRegex = /[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}/g;
+                const key = text.match(emailRegex);
+                localStorage.removeItem(key);
+                console.log(key);
+                ul.removeChild(li);
+        }  
+    }
+    
+    window.addEventListener("load",() =>{
+        displayUsers();
+    })
 });
